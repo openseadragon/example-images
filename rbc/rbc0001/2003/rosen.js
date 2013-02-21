9eb15d45cea356001cacc43b921819f1bf397ae3
thatcher <thatcher.christopher@gmail.com>
2013-02-21 08:14:50 -0500

    corrected path in example legacy image pyramid paths

--- rbc/rbc0001/2003/rosen.js
+++ rbc/rbc0001/2003/rosen.js
@@ -1,15 +1,15 @@
 rosen({
 	type: 'legacy-image-pyramid',
 	levels:[{
-	    url: '/openseadragon/examples/images/rbc/rbc0001/2003/2003rosen1799/0001q.jpg',
+	    url: '/example-images/rbc/rbc0001/2003/2003rosen1799/0001q.jpg',
 	    height: 889,
 	    width:  600
 	},{
-	    url: '/openseadragon/examples/images/rbc/rbc0001/2003/2003rosen1799/0001r.jpg',
+	    url: '/example-images/rbc/rbc0001/2003/2003rosen1799/0001r.jpg',
 	    height: 2201,
 	    width:  1485
 	},{
-	    url: '/openseadragon/examples/images/rbc/rbc0001/2003/2003rosen1799/0001v.jpg',
+	    url: '/examples-images/rbc/rbc0001/2003/2003rosen1799/0001v.jpg',
 	    height: 4402,
 	    width:  2970
 	}]
